7db3448fa397c783b2dca40d420c62bd30a3f553
boss0077 <sudarshanrpgowda7@gmail.com>
2025-08-04 16:52:13 +0530

    edit in output and main with region

--- bkend/main.tf
+++ bkend/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket-suda"
+  bucket = "demo-terraform-state-s3-bucket-suda"
 
   lifecycle {
     prevent_destroy = false
